8f99fde4be8f8894eeccf7428f030af935925567
Nata73gul <natagul.1973@gmail.com>
2024-10-10 13:55:30 +0300

    Add files via upload

---
+++ modul_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('В каком городе ты живешь?: ')  # город Симферополь
+print(len(my_string))  # кол-во символов
+print(my_string.upper())  # верхний регистр
+print(my_string.lower())  # нижний регистр
+print(my_string.replace(' ', ''))  # убрать пробелы
+print(my_string[0])  # первый символ
+print(my_string[-1])  # последний символ
